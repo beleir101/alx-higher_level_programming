8a7888d24befb7cbd881b76b2471c3595eaeab60
beleir101 <beleirmessay2436@gmail.com>
2023-03-24 10:40:49 +0300

    committed to this life

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,9 +2,7 @@
 
 def fizzbuzz():
     for i in range(1, 101):
-        if i == 100:
-            print("Buzz")
-        elif i % 3 == 0 and i % 5 != 0:
+        if i % 3 == 0 and i % 5 != 0:
             print("Fizz", end=" ")
         elif i % 3 != 0 and i % 5 == 0:
             print("Buzz", end=" ")
