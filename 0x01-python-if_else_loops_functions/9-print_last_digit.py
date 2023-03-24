e168927b24b90e2edfa7044473fa1b60390cdaa0
beleir101 <beleirmessay2436@gmail.com>
2023-03-24 10:20:36 +0300

    committed to this life

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def print_last_digit(number):
-    num = str(number)
-    leng = len(num)
-    print(num[leng-1], end="")
-    return num[leng-1]
+    if type(number) == int:
+        num = str(number)
+        leng = len(num)
+        print(num[leng-1], end="")
+        return num[leng-1]
