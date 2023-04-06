34b6b749987d5267a9161a9759d55cef625b9c5a
beleir101 <beleirmessay2436@gmail.com>
2023-04-06 20:11:08 +0300

    im shit

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,7 +16,7 @@ def add_integer(a, b=98):
         if type(a) is not float:
             raise TypeError("a must be an integer")
     if type(b) is not int:
-        type(b) is not float:
+        if type(b) is not float:
             raise TypeError("b must be an integer")
     else:
         a = int(a)
